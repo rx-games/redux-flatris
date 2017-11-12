6cbb3734f804a365520f1116864a7a98e80a2d5b
Krishna Sagar R <ragas.sagar.474@gmail.com>
2017-11-12 22:35:26 +0000

    config: add redux dev tools extension

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import { render } from "react-dom";
 import { createStore } from "redux";
 import { Provider, connect } from "react-redux";
 
-const notheingReducer = (state, action) => state;
+const colorReducer = (state, action) => state;
 
-const store = createStore(notheingReducer, { brick: { color: "sky" } });
+const store = createStore(
+  colorReducer,
+  { brick: { color: "sky" } },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const Brick = props => <div className={`brick ${props.color}`} />;
 
@@ -18,7 +22,7 @@ const BrickContainer = connect(
 )(Brick);
 
 render(
-  <Provider store={store} >
+  <Provider store={store}>
     <BrickContainer />
   </Provider>,
   document.getElementById("game")
