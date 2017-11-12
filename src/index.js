d557c472875beb370683f09e1609efee72ba5bc2
Krishna Sagar R <ragas.sagar.474@gmail.com>
2017-11-12 22:02:06 +0000

    config: add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
-import { render } from 'react-dom';
+import { render } from "react-dom";
 
-const Brick = props => <div className="brick" />;
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
 
-render(<Brick />, document.getElementById("game"));
+const notheingReducer = (state, action) => state;
+
+const store = createStore(notheingReducer, { brick: { color: "sky" } });
+
+const Brick = props => <div className={`brick ${props.color}`} />;
+
+const BrickContainer = connect(
+  state => ({
+    color: state.brick.color
+  }),
+  null
+)(Brick);
+
+render(
+  <Provider store={store} >
+    <BrickContainer />
+  </Provider>,
+  document.getElementById("game")
+);
