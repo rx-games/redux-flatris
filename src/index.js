b94c9fa008e1095f813fb59e5757dc5eacd00071
Krishna Sagar R <ragas.sagar.474@gmail.com>
2017-11-12 21:33:38 +0000

    config: upgrade react to 16.1.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from 'react-dom';
 
 const Brick = props => <div className="brick" />;
 
-ReactDOM.render(<Brick />, document.getElementById("game"));
+render(<Brick />, document.getElementById("game"));
